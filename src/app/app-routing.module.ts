4ad1163c1fd47849595c8e931a4a9f13398012cf
Mesut Eren <mesut.eren@outlook.com>
2022-07-18 00:28:26 +0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './admin/home/home.component';
+import { LayoutsComponent } from './admin/layouts/layouts.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: LayoutsComponent,
+    children: [
+      {
+        path:'',
+        component:HomeComponent,
+        loadChildren:()=> import('./admin/home/home.module').then(m=> m.HomeModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
